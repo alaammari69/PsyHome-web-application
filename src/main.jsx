2b69a8c9aab0f26d5085c1afb6d21ebca7389003
Ala Ammeri <ala.ammeri@proton.me>
2026-06-10 11:58:47 +0100

    DONE

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage/>}/>
         <Route path="/patients" element={<AllPatientsPage />} />
